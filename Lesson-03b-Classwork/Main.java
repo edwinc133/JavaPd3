624c307b09eed4c61c066db3bc66e905de6d13ed
edwinc133 <edwinc7336@nycstudents.net>
2025-09-30 13:30:53 +0000

    #

---
+++ Lesson-03b-Classwork/Main.java
@@ -0,0 +1,17 @@
+class Main {
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+
+  // Init function which runs when the 
+  // program begins.  
+
+  void init(){
+    System.out.println("Edwin Coreas Parada");
+    System.out.println("Edwin Coreas Parada\nFlushing");
+    System.out.println("Edwin Coreas Parada\tFlushing");
+    
+  }
+
+  
+}
