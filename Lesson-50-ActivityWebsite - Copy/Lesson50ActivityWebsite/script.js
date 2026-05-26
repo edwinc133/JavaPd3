2f0ed8fa3dd416bdb0fe1c0847232ed45d44fb34
edwinc133 <edwinc7336@nycstudents.net>
2026-05-26 13:27:07 +0000

    uuuuuuuuuuuuuuuuuuuuuuuuu

--- Lesson-50-ActivityWebsite - Copy/Lesson50ActivityWebsite/script.js
+++ Lesson-50-ActivityWebsite - Copy/Lesson50ActivityWebsite/script.js
@@ -1,8 +1,38 @@
 
-let data,songs;
+let data,players;
 function init(){
   $.ajaxSetup({async: false});
+  let link = "https://upgraded-potato-x57xvq7wg99fvq7q-8500.app.github.dev/"
+  let route = "soccer"
+  let info = await fetch(link+route)
+  soccer = await info.json()
+  console.log(soccer)
+
+
+
   
+  generateCards(soccer)
+
+  }
+
+  function generateCards(soccer){
+    let output = document.getElementById("output");
+    let build = "";
+
+    for(let x=0; x<soccer.length; x++){
+      let song = soccer[x]; 
+      build += `<div class="card">`
+      build += `<h3> Song Name </h3>`; 
+      build += `<p> Name: ${soccer.Name} </p>`;
+      build += `<h3> Album </h3>`;
+      build += `<p> Album: ${soccer.Album} </p>`;
+      build += `<h3> Composer </h3>`;
+      build += `<p> Composer: ${soccer.Composer} </p>`;
+      build += `</div>`
+
+    }
+    output.innerHTML = build;
+  }
  
 }
 
